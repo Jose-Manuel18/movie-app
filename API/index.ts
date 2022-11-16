9324ade311f1990fe771293ba44537ff2a5cb0d7
Jose-Manuel18 <103284630+Jose-Manuel18@users.noreply.github.com>
2022-11-16 09:29:39 -0400

    added more stuff

---
+++ API/index.ts
@@ -0,0 +1,5 @@
+export { MoviesAPI } from "./PopularMoviesAPI";
+export { SearchAPI } from "./SearchAPI";
+export { SeriesAPI } from "./SeriesAPI";
+export { TopRatedMoviesAPI } from "./TrendingMoviesAPI";
+export { UpComingAPI } from "./UpComingAPI";
